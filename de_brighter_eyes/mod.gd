3874a84bd15da63e4afeb716947134db635fc992
DeadlyEssence01 <79292336+DeadlyEssence01@users.noreply.github.com>
2023-05-26 18:30:24 -0400

    Brighter Eyes added

---
+++ de_brighter_eyes/mod.gd
@@ -0,0 +1,26 @@
+extends ContentInfo
+
+
+func _init():
+	var battle_generic: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/battle/human_layers/head_generic.png")
+	battle_generic.take_over_path("res://sprites/characters/battle/human_layers/head_generic.png")
+	var battle_mask1: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/battle/human_layers/head_mask1.png")
+	battle_mask1.take_over_path("res://sprites/characters/battle/human_layers/head_mask1.png")
+	var battle_makeup2: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/battle/human_layers/head_makeup2.png")
+	battle_makeup2.take_over_path("res://sprites/characters/battle/human_layers/head_makeup2.png")
+	var battle_makeup1: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/battle/human_layers/head_makeup1.png")
+	battle_makeup1.take_over_path("res://sprites/characters/battle/human_layers/head_makeup1.png")
+	var battle_grumpy: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/battle/human_layers/head_grumpy.png")
+	battle_grumpy.take_over_path("res://sprites/characters/battle/human_layers/head_grumpy.png")
+#
+	var world_generic: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/world/human_layers/head_generic.png")
+	world_generic.take_over_path("res://sprites/characters/world/human_layers/head_generic.png")
+	var world_mask1: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/world/human_layers/head_mask1.png")
+	world_mask1.take_over_path("res://sprites/characters/world/human_layers/head_mask1.png")
+	var world_makeup2: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/world/human_layers/head_makeup2.png")
+	world_makeup2.take_over_path("res://sprites/characters/world/human_layers/head_makeup2.png")
+	var world_makeup1: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/world/human_layers/head_makeup1.png")
+	world_makeup1.take_over_path("res://sprites/characters/world/human_layers/head_makeup1.png")
+	var world_grumpy: Resource = preload("res://mods/de_brighter_eyes/sprites/characters/world/human_layers/head_grumpy.png")
+	world_grumpy.take_over_path("res://sprites/characters/world/human_layers/head_grumpy.png")
+
