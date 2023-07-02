86839eb24b80650a4caa88cb99b76b194acd4027
DeadlyEssence01 <79292336+DeadlyEssence01@users.noreply.github.com>
2023-07-01 21:07:00 -0400

    Unlimited Romance Update v1.4

--- de_unlimited_romance/mod.gd
+++ de_unlimited_romance/mod.gd
@@ -1,5 +1,9 @@
 extends ContentInfo
 
+const MODUTILS: Dictionary = {
+	"updates": "https://gist.githubusercontent.com/DeadlyEssence01/0e27948d15fd6ce9f86a7a70cf9cbace/raw/DE_CB_Mod_Updater.json",
+}
+
 var unflag_romance: Reference = preload("res://mods/de_unlimited_romance/uncheck_romanced.gd").new()
 
 func _init():
